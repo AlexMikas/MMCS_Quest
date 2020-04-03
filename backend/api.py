9997440c2afb8f9ed093c57cd57094443cac71ac
Elena <metelica@sfedu.ru>
2020-04-03 18:53:30 +0300

    backend debuged

--- backend/api.py
+++ backend/api.py
@@ -5,7 +5,6 @@ import random
 from datetime import datetime, date, time
 from jinja2 import Environment, FileSystemLoader
 import uuid
-from flask import jsonify
 
 task_folder = "./backend/tasks"
 task_count = 3
@@ -16,7 +15,7 @@ def init_ids():
     for dir in range(task_count):
         ids = []
         for file in os.listdir(path.join(task_folder, str(dir))) :
-           with open(path.join(task_folder, str(dir), file)) as fd:
+           with open(path.join(task_folder, str(dir), file), encoding="utf-8") as fd:
                ids.append(json.load(fd)['id'])
         task_ids.append(ids)
     return task_ids
@@ -24,12 +23,12 @@ def init_ids():
 def fill_correct_ids():
     for dir in range(task_count):
         for file in os.listdir(path.join(task_folder, str(dir))) :
-            with open(path.join(task_folder, str(dir), file), 'r') as fd:
+            with open(path.join(task_folder, str(dir), file), encoding="utf-8") as fd:
                data = json.load(fd)
                id = generate_id_for_task_number(dir)
                data['id'] = str(id)
                file_name = F"task{id}.json"
-               with open(path.join(task_folder, str(dir), file_name), 'w') as f:
+               with open(path.join(task_folder, str(dir), file_name), 'w', encoding="utf-8") as f:
                     json.dump(data, f)
             os.remove(path.join(task_folder, str(dir), file))
 
@@ -57,12 +56,12 @@ def get_next_task_id_by_(next_task_num):
     return ids[ind]
                      
 def its_time():
-    dateZ = date(5, 4, 2020)
-    timeFrom = time(15, 0, 0)
+    dateZ = date(2020, 4, 3)
+    timeFrom = time(14, 0, 0)
     timeTo = time(23, 59, 59)
                                
-    dtFrom = datatime(date, timeFrom)
-    dtTo = datatime(date, timeTo)
+    dtFrom = datetime.combine(dateZ, timeFrom)
+    dtTo = datetime.combine(dateZ, timeTo)
                                
     dt = datetime.now()
     return dt > dtFrom and dt < dtTo
@@ -86,7 +85,7 @@ def fetch_task (id):
     if not path.exists(file_path):
         return None
     # get task
-    with open(file_path) as fd:
+    with open(file_path, encoding="utf-8") as fd:
         task = json.load(fd)
     return task
 
@@ -97,12 +96,13 @@ def fetch_super_task():
     if not path.exists(file_path):
         return None
     # get task
+    with open(file_path, encoding="utf-8") as fd:
+        task = json.load(fd)
     if its_time():
-        with open(file_path) as fd:
-            task = json.load(fd)
         return task
     else:
-        return jsonify()
+        task["text"] = "Задача станет доступна после 05.04.2020г. 14:00 по мск!"
+        return task
 
 # id - string
 def post_task (id):
@@ -111,8 +111,10 @@ def post_task (id):
         print("api >> post_task", id)
     task_json = fetch_task(id)
     if task_json:
-        return json.dumps(id=task_json["id"], text=task_json["text"], type=task_json["type"])
-    return jsonify()
+        desc_json = {"id": task_json["id"], "text": task_json["text"], "type": task_json["type"]}
+        return json.dumps(desc_json)
+    return "{}"
+
 
 def ordered(obj):
     if isinstance(obj, dict):
@@ -128,15 +130,28 @@ def post_solution (id, solution):
     if task_json:
         print("api >> post_solution", solution)
         print("api >> post_solution", task_json["solution"])
-        if ordered(task_json["solution"]) == ordered(solution):
+        res = {"success": False, "next_id": None}
+        if task_json["type"] == "equations":
+            for solu in task_json["solution"]:
+                if ordered(solu) == ordered(solution):
+                    next_task_num = get_task_number_by_(id) + 1
+                    res["success"] = True
+                    if next_task_num >= task_count:
+                        return json.dumps(res)
+                    next_id = get_next_task_id_by_(next_task_num)
+                    print("api >> post_solution", next_id, " ", next_task_num)
+                    res["next_task"] = json.loads(post_task(next_id))
+                    break
+        else:
             next_task_num = get_task_number_by_(id) + 1
+            res["success"] = True
             if next_task_num >= task_count:
-                return jsonify(success="true")
+                return json.dumps(res)
             next_id = get_next_task_id_by_(next_task_num)
             print("api >> post_solution", next_id, " ", next_task_num)
-            return jsonify(success="true", next_task=post_task(next_id))
-        return jsonify(success="false")
-    return jsonify()
+            res["next_task"] = json.loads(post_task(next_id))
+        return json.dumps(res)
+    return "{}"
 
 if __name__ == "__main__" :
     task_ids = init_ids()
